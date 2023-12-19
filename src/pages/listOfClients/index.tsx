9a98757f4f7e0f04e383cdf0cc865d0bde8ca5b9
Ajay Thakur <101165102+ajay-duple@users.noreply.github.com>
2023-12-19 17:40:49 +0530

    account LIst tabs

--- src/pages/listOfClients/index.tsx
+++ src/pages/listOfClients/index.tsx
@@ -1,7 +1,33 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Tab, Tabs, Typography } from "@mui/material";
+import * as React from "react";
 import AccountList from "../../components/listOfClients/AccountList";
 
+function TabPanel(props: any) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      className="p-0"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+      sx={{
+        padding: 0,
+      }}
+    >
+      {value === index && <Box sx={{ mt: 2 }}>{children}</Box>}
+    </div>
+  );
+}
+
 const ListOfClients = () => {
+  const [value, setValue] = React.useState("one");
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
   return (
     <>
       <Box sx={{ margin: " 1%" }}>
@@ -24,7 +50,28 @@ const ListOfClients = () => {
             Account List
           </Typography>
         </Box>
-        <AccountList />
+
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          textColor="secondary"
+          indicatorColor="secondary"
+          aria-label="secondary tabs example"
+        >
+          <Tab sx={{ color: "white" }} value="one" label="Super Url Admin" />
+          <Tab sx={{ color: "white" }} value="two" label="Expert" />
+        </Tabs>
+
+        <TabPanel value={value} index="one">
+          <AccountList />
+        </TabPanel>
+        <TabPanel value={value} index="two">
+          <Typography variant="h3" sx={{ color: "#fff", m: 2 }}>
+            Expert....
+          </Typography>
+          <AccountList />
+        </TabPanel>
+        {/* <AccountList /> */}
       </Box>
     </>
   );
