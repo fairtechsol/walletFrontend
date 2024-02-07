2a5e2d657fd6ff61d850dc533d2f082f0a7ad833
PardeepDuple <pradeep.duple@gmail.com>
2024-02-08 01:16:44 +0530

    fixed nan issue related to fgwallet

--- src/pages/walletSettings/index.tsx
+++ src/pages/walletSettings/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import ListItems from "../../components/walletSettings/ListItems";
+import { AppDispatch } from "../../store/store";
+import { useDispatch } from "react-redux";
+import { getUsersProfile } from "../../store/actions/user/userAction";
 
 const WalletSettings = () => {
+  const dispatch: AppDispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUsersProfile());
+  }, []);
   return <ListItems title={"Wallet"} />;
 };
 
