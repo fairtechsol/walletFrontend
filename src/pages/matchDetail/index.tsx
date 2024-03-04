f8ce1148a5a2afc9be189fa3898c567da343b5d3
PardeepDuple <pradeep.duple@gmail.com>
2024-03-04 18:56:07 +0530

    updates for palced bets

--- src/pages/matchDetail/index.tsx
+++ src/pages/matchDetail/index.tsx
@@ -101,7 +101,7 @@ const MatchDetail = () => {
       setMode(false);
       if (event?.matchId === state?.matchId) {
         dispatch(getMatchDetail(state?.matchId));
-        dispatch(getPlacedBets(state?.matchId));
+        dispatch(getPlacedBets(`eq${state?.matchId}`));
       }
     } catch (e) {
       console.log(e);
@@ -112,7 +112,7 @@ const MatchDetail = () => {
       setMode(false);
       if (event?.matchId === state?.matchId) {
         dispatch(getMatchDetail(state?.matchId));
-        dispatch(getPlacedBets(state?.matchId));
+        dispatch(getPlacedBets(`eq${state?.matchId}`));
       }
     } catch (e) {
       console.log(e);
@@ -168,7 +168,7 @@ const MatchDetail = () => {
     try {
       if (event?.matchId === state?.matchId) {
         dispatch(removeRunAmount(event));
-        dispatch(getPlacedBets(state?.matchId));
+        dispatch(getPlacedBets(`eq${state?.matchId}`));
       }
     } catch (error) {
       console.log(error);
@@ -179,7 +179,7 @@ const MatchDetail = () => {
     try {
       if (event?.matchId === state?.matchId) {
         dispatch(updateMaxLossForBetOnUndeclare(event));
-        dispatch(getPlacedBets(state?.matchId));
+        dispatch(getPlacedBets(`eq${state?.matchId}`));
       }
     } catch (error) {
       console.log(error);
@@ -190,7 +190,7 @@ const MatchDetail = () => {
     try {
       if (state?.matchId && profileDetail?.roleName) {
         dispatch(getMatchDetail(state?.matchId));
-        dispatch(getPlacedBets(state?.matchId));
+        dispatch(getPlacedBets(`eq${state?.matchId}`));
       }
     } catch (e) {
       console.log(e);
@@ -248,7 +248,7 @@ const MatchDetail = () => {
       if (document.visibilityState === "visible") {
         if (state?.matchId) {
           dispatch(getMatchDetail(state?.matchId));
-          dispatch(getPlacedBets(state?.matchId));
+          dispatch(getPlacedBets(`eq${state?.matchId}`));
         }
       } else if (document.visibilityState === "hidden") {
         socketService.match.leaveMatchRoom(state?.matchId);
