bb30d7737a9e29680e81f143f2ec7589f8b7f215
PardeepDuple <pradeep.duple@gmail.com>
2024-03-06 16:30:43 +0530

    removed showing active on offline

--- src/components/matchDetail/SessionMarket/SeasonMarketBox.tsx
+++ src/components/matchDetail/SessionMarket/SeasonMarketBox.tsx
@@ -87,7 +87,7 @@ const SeasonMarketBox = (props: any) => {
             />
           )}
 
-          {newData?.status !== "active" || newData?.activeStatus !== "live" ? (
+          {newData?.status !== "active" ? (
             <Box
               sx={{
                 background: "rgba(0,0,0,1)",
