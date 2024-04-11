f64ec68533bfa7db37a02e164ad5b981f5ff217b
PardeepDuple <pradeep.duple@gmail.com>
2024-04-11 13:48:12 +0530

    null operator for socket

--- src/socketManager/matchDetailService.tsx
+++ src/socketManager/matchDetailService.tsx
@@ -2,7 +2,7 @@ import { socket, thirdParty } from ".";
 
 export const matchSocketService = {
   joinMatchRoom: (matchId: any, roleName: any) => {
-    socket.emit("matchRoom", {
+    socket?.emit("matchRoom", {
       id: matchId,
     });
 
@@ -12,92 +12,92 @@ export const matchSocketService = {
     });
   },
   leaveAllRooms: () => {
-    socket.emit("leaveAll");
+    socket?.emit("leaveAll");
   },
   leaveMatchRoom: (matchId: any) => {
-    thirdParty.emit("disconnectCricketData", {
+    thirdParty?.emit("disconnectCricketData", {
       matchId: matchId,
     });
   },
   getMatchRates: (matchId: any, callback: any) => {
-    thirdParty.on(`liveData${matchId}`, callback);
+    thirdParty?.on(`liveData${matchId}`, callback);
   },
   matchResultDeclared: (callback: any) => {
-    socket.on("matchResult", callback);
+    socket?.on("matchResult", callback);
   },
   matchResultUnDeclared: (callback: any) => {
-    socket.on("matchResultUnDeclare", callback);
+    socket?.on("matchResultUnDeclare", callback);
   },
   declaredMatchResultAllUser: (callback: any) => {
-    socket.on("matchResultDeclareAllUser", callback);
+    socket?.on("matchResultDeclareAllUser", callback);
   },
   unDeclaredMatchResultAllUser: (callback: any) => {
-    socket.on("matchResultUnDeclareAllUser", callback);
+    socket?.on("matchResultUnDeclareAllUser", callback);
   },
   matchDeleteBet: (callback: any) => {
-    socket.on("matchDeleteBet", callback);
+    socket?.on("matchDeleteBet", callback);
   },
   sessionDeleteBet: (callback: any) => {
-    socket.on("sessionDeleteBet", callback);
+    socket?.on("sessionDeleteBet", callback);
   },
   matchAdded: (callback: any) => {
-    socket.on("addMatch", callback);
+    socket?.on("addMatch", callback);
   },
   updateUserBalance: (callback: any) => {
-    socket.on("updateUserBalance", callback);
+    socket?.on("updateUserBalance", callback);
   },
   userBalanceUpdate: (callback: any) => {
-    socket.on("userBalanceUpdate", callback);
+    socket?.on("userBalanceUpdate", callback);
   },
   userSessionBetPlaced: (callback: any) => {
-    socket.on("userSessionBetPlaced", callback);
+    socket?.on("userSessionBetPlaced", callback);
   },
   userMatchBetPlaced: (callback: any) => {
-    socket.on("userMatchBetPlaced", callback);
+    socket?.on("userMatchBetPlaced", callback);
   },
   sessionResult: (callback: any) => {
-    socket.on("sessionResult", callback);
+    socket?.on("sessionResult", callback);
   },
   sessionResultUnDeclare: (callback: any) => {
-    socket.on("sessionResultUnDeclare", callback);
+    socket?.on("sessionResultUnDeclare", callback);
   },
   sessionResultUnDeclareOff: () => {
-    socket.off("sessionResultUnDeclare");
+    socket?.off("sessionResultUnDeclare");
   },
   sessionResultOff: () => {
-    socket.off("sessionResult");
+    socket?.off("sessionResult");
   },
   userSessionBetPlacedOff: () => {
-    socket.off("userSessionBetPlaced");
+    socket?.off("userSessionBetPlaced");
   },
   userMatchBetPlacedOff: () => {
-    socket.off("userMatchBetPlaced");
+    socket?.off("userMatchBetPlaced");
   },
   matchResultDeclaredOff: () => {
-    socket.off("matchResult");
+    socket?.off("matchResult");
   },
   matchResultUnDeclaredOff: () => {
-    socket.off("matchResultUnDeclare");
+    socket?.off("matchResultUnDeclare");
   },
   declaredMatchResultAllUserOff: () => {
-    socket.off("matchResultDeclareAllUser");
+    socket?.off("matchResultDeclareAllUser");
   },
   unDeclaredMatchResultAllUserOff: () => {
-    socket.off("matchResultUnDeclareAllUser");
+    socket?.off("matchResultUnDeclareAllUser");
   },
   matchDeleteBetOff: () => {
-    socket.off("matchDeleteBet");
+    socket?.off("matchDeleteBet");
   },
   sessionDeleteBetOff: () => {
-    socket.off("sessionDeleteBet");
+    socket?.off("sessionDeleteBet");
   },
   updateUserBalanceOff: () => {
-    socket.off("updateUserBalance");
+    socket?.off("updateUserBalance");
   },
   matchAddedOff: () => {
-    socket.off("addMatch");
+    socket?.off("addMatch");
   },
   getMatchRatesOff: (matchId: any) => {
-    thirdParty.off(`liveData${matchId}`);
+    thirdParty?.off(`liveData${matchId}`);
   },
 };
