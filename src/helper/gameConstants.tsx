9be354c783c81e71c56c4c18f2b9698d51d52ff1
dhanesh241 <143992210+dhanesh241@users.noreply.github.com>
2024-04-12 11:46:45 +0530

    match icon on inplay

---
+++ src/helper/gameConstants.tsx
@@ -0,0 +1,13 @@
+import { Cricket, Football,Tennis } from "../assets";
+
+export const IconConstants: any = {
+  cricket: Cricket,
+  football: Football,
+  tennis: Tennis,
+};
+export const MarketTypeConstants: any = {
+  MatchOdd: "matchOdd",
+  HalfTime: "halfTime",
+  FirstHalfGoal: "firstHalfGoal",
+  OverUnder: "overUnder",
+};
