4b702b4c7ca5b3dd1bf2297fb02ad83008d2cae5
PardeepDuple <pradeep.duple@gmail.com>
2024-05-28 18:21:34 +0530

    removed extra code

--- src/components/horseRacingComp/RacingListComponent.tsx
+++ src/components/horseRacingComp/RacingListComponent.tsx
@@ -10,38 +10,6 @@ import {
 import moment from "moment";
 import { useNavigate } from "react-router-dom";
 
-const data = [
-  {
-    id: 1,
-    location: "Brighton",
-    times: ["18:50", "19:20", "19:50", "20:20", "20:50", "21:25"],
-  },
-  {
-    id: 2,
-    location: "Hexham",
-    times: ["21:55", "22:28", "23:28", "23:58", "00:28", "01:30"],
-  },
-  {
-    id: 3,
-    location: "Nottingham",
-    times: [
-      "18:40",
-      "19:10",
-      "19:40",
-      "20:10",
-      "20:40",
-      "21:10",
-      "21:40",
-      "22:15",
-    ],
-  },
-  {
-    id: 4,
-    location: "Huntingdon",
-    times: ["22:40", "23:10", "00:10", "00:40", "01:10"],
-  },
-];
-
 const YellowButton = styled("button")(() => ({
   backgroundColor: "#F8C851",
   color: "black",
