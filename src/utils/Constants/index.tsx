dae3ca3f92cb8b2267adeb784f11dadc4cd06fb4
PardeepDuple <pradeep.duple@gmail.com>
2024-08-03 16:42:59 +0530

    updated constants for cricket match 20 20 and test teenpatti

--- src/utils/Constants/index.tsx
+++ src/utils/Constants/index.tsx
@@ -2,6 +2,7 @@ import {
   AndarBahar2,
   CasinoWar,
   Cricket,
+  CriketMatch2020,
   DragonTiger20,
   Five5,
   Football,
@@ -279,6 +280,8 @@ export const gameIconConstants = {
   [cardGamesTypeConstants.poker]: Poker,
   [cardGamesTypeConstants.poker6]: Poker,
   [cardGamesTypeConstants.poker20]: Poker,
+  [cardGamesTypeConstants.teen9]: Teenpatti20,
+  [cardGamesTypeConstants.cmatch20]: CriketMatch2020,
 };
 
 export const profitLossDataForMatchConstants = {
