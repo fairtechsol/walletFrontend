e0d9e8ffcd9e570576d9d2c8a2cd50e71816a135
dhanesh241 <143992210+dhanesh241@users.noreply.github.com>
2024-10-15 16:19:33 +0530

    fix

--- src/components/matchDetail/SessionMarket/SeasonMarketBox.tsx
+++ src/components/matchDetail/SessionMarket/SeasonMarketBox.tsx
@@ -10,7 +10,7 @@ const SeasonMarketBox = (props: any) => {
   const { newData, setData, profitLossData, index, type } = props;
   const theme = useTheme();
   const matchesMobile = useMediaQuery(theme.breakpoints.down("lg"));
-console.log('first',type)
+// console.log('first',type)
   return (
     <>
       <Box
