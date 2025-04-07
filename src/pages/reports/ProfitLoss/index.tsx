cf5af5fbdb42d7471ecdae56e2f13244265a43db
Pardeepfairtech <pardeepfairtech@gmail.com>
2025-04-07 12:10:22 +0530

    refactor: remove unused Box import from ProfitLossReport component

--- src/pages/reports/ProfitLoss/index.tsx
+++ src/pages/reports/ProfitLoss/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { debounce } from "lodash";
 import moment from "moment";
 import { useEffect, useMemo, useState } from "react";
