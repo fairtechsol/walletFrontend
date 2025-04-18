173593e71cc325b9c87f576d62253fe5810bfe86
Pardeepfairtech <pardeepfairtech@gmail.com>
2025-04-18 15:36:19 +0530

    refactor: replace MAC88 PNG image with WEBP format for improved performance

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -51,7 +51,7 @@ export { default as LoginBg } from "./images/loginBg_webp.webp";
 export { default as FgLogo } from "./images/logo02.webp";
 export { default as Logout } from "./images/logout.png";
 export { default as Lucky7A } from "./images/lucky7A.png";
-export { default as MAC88 } from "./images/mac88.png";
+export { default as MAC88 } from "./images/mac88.webp";
 export { default as mail } from "./images/mail.png";
 export { default as MYACCOUNT } from "./images/my-account.webp";
 export { default as NOT } from "./images/not.png";
