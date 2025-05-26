f73d37f00986e8ef92d3d151941e30672a5905ba
Pardeepfairtech <pardeepfairtech@gmail.com>
2025-05-26 15:17:22 +0530

    refactor: extract BackLayBox component into its own file for better modularity

--- src/components/Inplay/BackLayRow.tsx
+++ src/components/Inplay/BackLayRow.tsx
@@ -1,28 +1,5 @@
-import { Box, Typography } from "@mui/material";
-
-interface BackLayBoxProps {
-  color: string;
-  type: string;
-}
-
-const BackLayBox = ({ color, type }: BackLayBoxProps) => {
-  return (
-    <Box
-      sx={{
-        background: color,
-        width: { lg: "20%", xs: "25%" },
-        height: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Typography sx={{ fontSize: "12px", color: "#000", fontWeight: "600" }}>
-        {type}
-      </Typography>
-    </Box>
-  );
-};
+import { Box } from "@mui/material";
+import BackLayBox from "./BackLayBox";
 
 const BackLayRow = () => {
   return (
