6894c73d7f3f6fdee877de07d5fee246610fe76a
Pardeepfairtech <pardeepfairtech@gmail.com>
2025-05-27 11:10:13 +0530

    refactor: simplify apiMatchMap key assignment logic for improved readability

--- src/store/reducers/match/matchListSlice.tsx
+++ src/store/reducers/match/matchListSlice.tsx
@@ -149,8 +149,8 @@ const matchListSlice = createSlice({
           const apiMatchMap = new Map();
 
           matchListFromApi.forEach((item: any) => {
-            if (item?.beventId) apiMatchMap.set(+item.beventId, item);
-            if (item?.gmid) apiMatchMap.set(+item.gmid, item);
+            const id = Number(item.beventId || item.gmid);
+            apiMatchMap.set(id, item);
           });
 
           state.matchListInplay.matches = state.matchListInplay.matches.map(
