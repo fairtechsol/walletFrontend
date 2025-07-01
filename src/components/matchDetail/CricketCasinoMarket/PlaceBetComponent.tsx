37006619a683fc70ac97f2ee62ce190f2fa52abb
PardeepFairtechh <pardeep.kumar@fairtechsol.com>
2025-07-01 17:30:30 +0530

    Fix profitLoss calculation in PlaceBetComponent to handle NaN values

--- src/components/matchDetail/CricketCasinoMarket/PlaceBetComponent.tsx
+++ src/components/matchDetail/CricketCasinoMarket/PlaceBetComponent.tsx
@@ -45,8 +45,9 @@ const PlaceBetComponent = ({ profitLoss, index }: PlaceBetComponentProps) => {
               ? "Profit/Loss"
               : handleNumber(
                   parseFloat(
-                    profitLoss?.betPlaced?.[index] ||
-                      profitLoss?.betPlaced?.[index]?.profitLoss ||
+                    (isNaN(profitLoss?.betPlaced?.[index])
+                      ? profitLoss?.betPlaced?.[index]?.profitLoss
+                      : profitLoss?.betPlaced?.[index]) ||
                       profitLoss?.profitLoss?.[index]
                   ).toFixed(2),
                   ""
